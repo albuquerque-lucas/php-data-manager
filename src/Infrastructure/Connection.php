be32e9e127e064607a2c9953b3d89acf12367de2
albuquerque-lucas <lucaslpra@gmail.com>
2023-07-31 19:42:04 -0300

    Adicionando configuracoes de ambiente e gitignore

--- src/Infrastructure/Connection.php
+++ src/Infrastructure/Connection.php
@@ -9,11 +9,11 @@ class Connection
 {
     public static function connect()
     {
-        $hostName = 'containers-us-west-89.railway.app';
-        $port = '7241';
-        $dbName = 'railway';
-        $userName = 'root';
-        $password = 'FUdfoWuE5r1DJxJTBwhQ';
+        $hostName = $_ENV['DB_HOST'];
+        $port = $_ENV['DB_PORT'];
+        $dbName = $_ENV['DB_NAME'];
+        $userName = $_ENV['DB_USERNAME'];
+        $password = $_ENV['DB_PASSWORD'];
         try {
             return new PDO("mysql:host=$hostName;port=$port;dbname=$dbName", $userName, $password);
         } catch (PDOException $PDOException) {
