7ba12b2e5995bb816e78cf83024cf82c72dc56ee
albuquerque-lucas <lucaslpra@gmail.com>
2023-08-16 21:50:54 -0300

    Pagina inicial com dumps para apurar as variaveis no servidor de hospedagem

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -10,12 +10,20 @@ class HomeController
 
     public function renderHomeRequest()
     {
-      $homeView = new HomeView();
-      $sessionData = SessionManager::getSessionData();
-      list($userData) = $sessionData;
-      $homeView->renderHomeView('home.phtml', [
-        'status' => $userData['status'],
-        'user' => $userData['user'],
-      ]);
+      echo "Pagina inicial";
+      var_dump($_SERVER);
+      echo "Dump somente da Request URI:";
+      var_dump($_SERVER['REQUEST_URI']);
+      echo "Fazer um dump de PATH INFO se houver";
+      var_dump($_SERVER['PATH_INFO']);
+      echo "Dump do PHP Self:";
+      var_dump($_SERVER['PHP_SELF']);
+      // $homeView = new HomeView();
+      // $sessionData = SessionManager::getSessionData();
+      // list($userData) = $sessionData;
+      // $homeView->renderHomeView('home.phtml', [
+      //   'status' => $userData['status'],
+      //   'user' => $userData['user'],
+      // ]);
     }
 }
